cd5ccd86e3e403069f563258b633a15781f4fa5f
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-07-11 23:57:28 +0300

    Add current weather component

---
+++ src/app/weather/current-weather/current-weather.component.ts
@@ -0,0 +1,82 @@
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
+import { WeatherService } from '../weather.service';
+import { Subject, takeUntil } from 'rxjs';
+import { Weather } from '../../models/Weather';
+import { CardModule } from 'primeng/card';
+import { DecimalPipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
+import { Button } from 'primeng/button';
+import { WeatherSearchComponent } from '../weather-search/weather-search.component';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { ToggleSwitch } from 'primeng/toggleswitch';
+import { FormsModule } from '@angular/forms';
+import { TempratureConvertPipe } from '../../pipes/temprature-convert.pipe';
+
+@Component({
+  selector: 'app-current-weather',
+  imports: [
+    CardModule,
+    DecimalPipe,
+    DatePipe,
+    AsyncPipe,
+    Button,
+    WeatherSearchComponent,
+    ProgressSpinnerModule,
+    ToggleSwitch,
+    FormsModule,
+    TempratureConvertPipe
+  ],
+  templateUrl: './current-weather.component.html',
+  styleUrl: './current-weather.component.css',
+})
+export class CurrentWeatherComponent {
+  weatherService = inject(WeatherService);
+  currentWeather$ = this.weatherService.currentWeather$;
+  loading$ = this.weatherService.loading$;
+  error$ = this.weatherService.error$;
+  switchTemp: boolean = false;
+
+  private destroy$ = new Subject<void>();
+
+  getTimeInMs(timestamp: number): number {
+    return timestamp * 1000;
+  }
+
+  get temperatureUnit(): 'C' | 'F' {
+    return this.switchTemp ? 'F' : 'C';
+  }
+
+
+  ngOnInit(): void {
+    this.loadWeatherData();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private loadWeatherData(): void {
+    this.weatherService
+      .getUserLocationWeather()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (weather: Weather) => {
+          console.log('Weather loaded:', weather);
+        },
+        error: (error: string) => {
+          console.error('Error loading weather:', error);
+        },
+      });
+  }
+
+  getLocalTime(dt: number, timezoneOffset: number): Date {
+    return new Date((dt + timezoneOffset) * 1000);
+  }
+
+
+  retry(): void {
+    this.loadWeatherData();
+  }
+}
